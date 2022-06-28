c33846591ddb69a145dc1bae9450b28b7355e73c
daydreamingmari <96265133+sleepin-owl@users.noreply.github.com>
2022-06-28 22:07:34 +0200

    Update 2_🔍_Search.py

--- pages/2_🔍_Search.py
+++ pages/2_🔍_Search.py
@@ -36,8 +36,7 @@ def style():
     
     st.markdown("<h2 class='title-search';'> Ricerca la canzone</h2>", unsafe_allow_html=True)
     selected_song = st.selectbox(
-                ('Inserisci il nome della canzone', tracks_data['song_name'])
-                default="Style"
+                ('Inserisci il nome della canzone', tracks_data['song_name'], default = "Style")
     )
     selected_song_row = tracks_data[tracks_data['song_name'] == selected_song]
     # st.write(selected_song_row)
